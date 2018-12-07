91db9fb37b1252374c252f74e14e17e01d4fa070
ulopeznovoa <ulopeznovoa@gmail.com>
2018-12-07 16:03:53 +0000

    Creating folders

--- server.py
+++ server.py
@@ -5,17 +5,27 @@ app = Flask(__name__)
 
 @app.route('/createBids', methods = ['POST'])
 def postJsonHandler():
- 
-    print (request.is_json)
+
+    if request.is_json == False:
+        raise RuntimeError("Incorrect body message -- not a JSON file")
+
     content = request.get_json()
-    print (content['message'])   
+    
+    ### Create DICOM folder structure
 
-    # Create DICOM folder structure
+    # Create temporary folder to arrange DICOM files
+    folder_name = 'bids_temp_'+str(time.time())
+    try:
+        os.mkdir('/tmp/'+folder_name)
+    except FileExistsError:
+        print("Directory ",folder_name, " already exists")
 
-    # Run bidskit 
 
 
-    return 'JSON posted'
+    ### Run bidskit 
+
+
+    return 'Create BIDS finished'
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
