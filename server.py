80eaa52ef8b46b55cdcbea552597f6659841d729
ulopeznovoa <ulopeznovoa@gmail.com>
2018-12-07 16:45:54 +0000

    Local DICOM folder creation

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import request
 
+from shutil import copytree
+import os
+import time
+
 app = Flask(__name__)
 
 @app.route('/createBids', methods = ['POST'])
@@ -9,23 +13,33 @@ def postJsonHandler():
     if request.is_json == False:
         raise RuntimeError("Incorrect body message -- not a JSON file")
 
-    content = request.get_json()
+    data = request.get_json()
     
     ### Create DICOM folder structure
 
     # Create temporary folder to arrange DICOM files
-    folder_name = 'bids_temp_'+str(time.time())
+    parent_folder = '/tmp/bids_temp_'+str(time.time())
+
     try:
-        os.mkdir('/tmp/'+folder_name)
+        os.mkdir(parent_folder)
     except FileExistsError:
-        print("Directory ",folder_name, " already exists")
+        print("Directory ",parent_folder, " already exists")
 
+    # Create and populate DICOM subfolder
+    os.mkdir(parent_folder+'/dicom')
 
+    for sub in data['scans']:
+        os.mkdir(parent_folder+'/dicom/sub-'+sub)
+        for ses in data['scans'][sub]:
+            copytree(data['scans'][sub][ses], parent_folder+'/dicom/sub-'+sub+'/ses-'+ses)
 
     ### Run bidskit 
 
 
-    return 'Create BIDS finished'
+
+
+
+    return 'CreateBIDS finished'
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
