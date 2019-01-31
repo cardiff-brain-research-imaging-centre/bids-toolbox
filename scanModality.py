b846d97a613881d2a08d380cbdbdfb443bea7584
ulopeznovoa <ulopeznovoa@gmail.com>
2019-01-31 11:47:27 -0500

    1st version Scan modality detection algorithm

--- scanModality.py
+++ scanModality.py
@@ -15,7 +15,7 @@ def inferScanModality(tag, parent_folder):
     ## Take first session, first subject as reference
     nifti_dir = glob.glob(parent_folder+'/work/conversion/sub*/ses*')[0]    
 
-    ## If bval and bvec files exist for a given tag, its a difussion scan
+    ## If bval and bvec files exist for a given tag, its a diffusion scan
     if((len(glob.glob(nifti_dir+'/*'+tag+'*.bvec')) > 0) and (len(glob.glob(nifti_dir+'/*'+tag+'*.bval')) > 0)):
         scan['type'] = 'dwi'
         scan['modality'] = 'dwi'
@@ -26,12 +26,50 @@ def inferScanModality(tag, parent_folder):
             with open(sidecar_file, 'r') as f:
                 sidecar_data = json.load(f)
 
-            ## To-Do: Process sidecar data to detect anat or func
-            scan['type'] = 'anat'
-            scan['modality'] = 'T1w'
+            FA = sidecar_data['FlipAngle']
+            TE = sidecar_data['EchoTime']
+            TR = sidecar_data['RepetitionTime']
+
+            if TE < 30:
+                if FA == 90:
+                    if TR < 800:
+                        scan['type'] = 'anat'
+                        scan['modality'] = 'T1w'                       
+                    else:
+                        scan['type'] = 'anat'
+                        scan['modality'] = 'PD'
+                elif FA >= 5 and FA <= 20:
+                    scan['type'] = 'anat'
+                    scan['modality'] = 'T1w'   
+                else:
+                    scan['type'] = 'anat'
+                    scan['modality'] = 'T2star'     
+
+            elif TE >= 30 and TE <= 60:
+                scan['type'] = 'func'
+                scan['modality'] = 'BOLD'
+
+            elif TE > 60 and TE <= 80:
+                if FA == 90:
+                    scan['type'] = 'anat'
+                    scan['modality'] = 'T2w'     
+                else:
+                    scan['type'] = 'IR'
+                    scan['modality'] = 'STIR'   
+        
+            else:
+                if TR >= 2000 and TR <= 3000:
+                    scan['type'] = 'anat'
+                    scan['modality'] = 'T2w'   
+                elif TR > 3000:
+                    scan['type'] = 'anat'
+                    scan['modality'] = 'FLAIR'   
+                else:
+                    scan['type'] = 'anat'
+                    scan['modality'] = 'T1w'
         
         else: # Branch to default
             scan['type'] = 'anat'
-            scan['modality'] = 'T2w'
+            scan['modality'] = 'T1w'
 
     return scan
